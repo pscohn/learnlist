7bd7a1be889120833f63a485093d657f45e2db2f
Paul Cohn <pscohn@gmail.com>
2019-05-11 01:07:38 -0400

    restore explore page

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -1,9 +1,11 @@
 class ExploreController < ApplicationController
   def recent
+    @user = current_user
     @lists = List.where(private: false).paginate(page: params[:page])
   end
 
   def popular
+    @user = current_user
     @lists = List.unscoped.where(private: false).order(saves: :desc).limit(100).paginate(page: params[:page])
   end
 end
